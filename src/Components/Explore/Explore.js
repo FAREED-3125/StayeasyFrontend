1d5df0b14256499bd8a3daad4eec2c8fea6622a1
FAREED-3125 <126556539+FAREED-3125@users.noreply.github.com>
2023-11-05 21:31:46 +0530

    Update Explore.js

--- src/Components/Explore/Explore.js
+++ src/Components/Explore/Explore.js
@@ -19,7 +19,7 @@ const Explore = () => {
         "https://i.pinimg.com/originals/72/4f/06/724f066edfefde900fab352e50cd6a54.jpg",
         "https://lp-cms-production.imgix.net/2019-06/f142b3016a4a4bb7bb7c20b1aa38a7c6-chennai-madras.jpg?auto=compress&fit=crop&fm=auto&sharp=10&vib=20&w=1200",
         "https://www.mistay.in/travel-blog/content/images/size/w2000/2020/06/cover-10.jpg",
-        "https://th.bing.com/th/id/R.9c247dfc6ccfc0d12bcfae0ea2cf3ddd?rik=3px%2frKhXR4AVHw&riu=http%3a%2f%2f3.bp.blogspot.com%2f_DBdASmjjss4%2fTFLqft8AUZI%2fAAAAAAAAAC0%2fIb9foHHtSL0%2fs1600%2fcharminar2.jpg&ehk=ZquPeG1EqtP90cihGHZvVuSVvT26z29aVl6HRQIr7oI%3d&risl=&pid=ImgRaw&r=0",
+        "https://media.istockphoto.com/id/1194408155/photo/charminar-hyderabad-background-with-copy-space.webp?b=1&s=170667a&w=0&k=20&c=3hfSYWzWzKDyLNXbP9h1Ow2PJRLUV-R4iyPxMH353UI=",
         "https://th.bing.com/th/id/OIP.QVSoLjd5S7wn4Aggi5Jp6AHaE8?pid=ImgDet&rs=1",
         "https://1.bp.blogspot.com/-uqv7sYU74Ho/Xgtlb2rRVEI/AAAAAAAADBs/Dcwg7IYfX6MFp84yHuXd9Vzy7xmvquT-wCLcBGAsYHQ/s1600/u.6.jpg",
         "https://www.adotrip.com/public/images/city/master_images/5e4d07bdd37f0-Pune_Travel.jpg"
